7f37eb74b91fddc7f0f210b130ba4e90b7f475a3
RollsRoycewk <50765560@qq.com>
2020-11-28 22:51:48 +0800

    feat:基本样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import "./styles/reset.css";
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
